87245a672fe48269fe332915620c154aba122814
kelvin <kelvindike18@gmail.com>
2023-05-07 12:44:52 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const male=9;
+const female =2;
+const isPresentAll=true;
+
+const noOfFM={
+    male,
+    female,
+};
+
+const colorsWorn=[ "black", "grey", "pink"]; 
+
+let add = male + female;
+let subtract = male - female ;
+let division = male / female ;
+let mode = male % female;
+
+console.log(mode, division, subtract, add);
+
+console.log(add++);
